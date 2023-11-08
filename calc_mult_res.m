4cb8af2074b51c3c540230a3aa77b005248cd9ea
Aditya Rastogi <53825123+adityaras@users.noreply.github.com>
2023-11-08 13:19:15 +0530

    Add files via upload

--- calc_mult_res.m
+++ calc_mult_res.m
@@ -1,7 +1,11 @@
 
-st1='E:\Desktop\CoSkel_Matlab_Codes\TransUNET_GT';
+% st1='E:\Desktop\CoSkel_Matlab_Codes\TransUNET_GT';
+% st3='E:\Desktop\CoSkel_Matlab_Codes\Results\tnet-sa-b4-alph5-cl2-gamma0-2-v57-58\save_gts';
 
-st3='E:\Desktop\CoSkel_Matlab_Codes\Results\tnet-sa-b4-alph5-cl2-gamma0-2-v57-58\save_gts';
+
+st1='/Users/adityarastogi/Desktop/Personal/CoSkel_Matlab_Codes-main/TransUNET_GT';
+st3='/Users/adityarastogi/Desktop/Personal/CoSkel_Matlab_Codes-main/Results/TRUE_SA_WID_MSE_DL_ALPH5_B4_NUM2_GAMMA_NOT02_DC/save_gts';
+
 
 addpath(st1)
 addpath(st3) 
@@ -9,7 +13,7 @@ addpath(st3)
 dr=dir([st3]);
 numRows = numel(dr) - 2;
 finalRes = cell(numRows, 8);
-disp("tnet-sa-b4-alph5-cl2-gamma0-2-v57-58")
+disp("TRUE_SA_WID_MSE_DL_ALPH5_B4_NUM2_GAMMA_NOT02_DC")
 dr=dir([st3]);
 for i=3:numel(dr)
     st2=fullfile(st3, dr(i).name);
